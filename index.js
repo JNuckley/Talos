59cec9ee0f241432b3878cf9698e54405abc2947
Richard <aagbajdos@gmail.com>
2020-01-09 14:10:05 +0000

    eslint: Added gitignore file node modules running eslint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Board, Led, Button } from 'johnny-five';
 
 // Set `lightOn` to true as a default since our LED will be on
@@ -5,12 +6,12 @@ let lightOn = true;
 
 // Make a new Board Instance
 const board = new Board();
-//When the board's connected, turn on the LED connected to pin 9
+// When the board's connected, turn on the LED connected to pin 9
 board.on('ready', function() {
   console.log('[johnny-five] Board is ready.');
 
   //Make a new Led object and connect it to pin 9
-const led = new Led(9);
+  const led = new Led(9);
 
   const pushButton = new Button({
     pin: 7,
@@ -35,9 +36,9 @@ const led = new Led(9);
       led.on();
       lightOn = true;
 
-  }
+    }
 
-});
+  });
   this.repl.inject({
     //Control the Led via calling for the objects
     led: led,
@@ -58,10 +59,10 @@ const led = new Led(9);
         led.stop().off();
         lightOn = false;
       }
-    }
+    },
   });
   // commands = led.on(9), led.off(9),led.pulse(2000), led.stop(9)
-  //When the board is closing, stop any LED animations and turn it off
+  // When the board is closing, stop any LED animations and turn it off
   this.on('exit', function() {
     led.stop().off();
     console.log('[johnny-five] Bye Bye.');
